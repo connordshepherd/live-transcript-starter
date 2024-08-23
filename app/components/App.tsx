8a978aae833d2f1ff75ec76b2d7aa623e39e4a64
Connor Shepherd <31867826+connordshepherd@users.noreply.github.com>
2024-08-22 17:13:43 -0700

    Update App.tsx

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -15,13 +15,10 @@ import {
 import Visualizer from "./Visualizer";
 
 const App: () => JSX.Element = () => {
-  const [caption, setCaption] = useState<string | undefined>(
-    "Powered by Deepgram"
-  );
+  const [transcriptions, setTranscriptions] = useState<string[]>(["Powered by Deepgram"]);
   const { connection, connectToDeepgram, connectionState } = useDeepgram();
   const { setupMicrophone, microphone, startMicrophone, microphoneState } =
     useMicrophone();
-  const captionTimeout = useRef<any>();
   const keepAliveInterval = useRef<any>();
 
   useEffect(() => {
@@ -55,21 +52,10 @@ const App: () => JSX.Element = () => {
     };
 
     const onTranscript = (data: LiveTranscriptionEvent) => {
-      const { is_final: isFinal, speech_final: speechFinal } = data;
-      let thisCaption = data.channel.alternatives[0].transcript;
+      const thisCaption = data.channel.alternatives[0].transcript;
 
-      console.log("thisCaption", thisCaption);
       if (thisCaption !== "") {
-        console.log('thisCaption !== ""', thisCaption);
-        setCaption(thisCaption);
-      }
-
-      if (isFinal && speechFinal) {
-        clearTimeout(captionTimeout.current);
-        captionTimeout.current = setTimeout(() => {
-          setCaption(undefined);
-          clearTimeout(captionTimeout.current);
-        }, 3000);
+        setTranscriptions(prev => [...prev, thisCaption]);
       }
     };
 
@@ -84,7 +70,6 @@ const App: () => JSX.Element = () => {
       // prettier-ignore
       connection.removeListener(LiveTranscriptionEvents.Transcript, onTranscript);
       microphone.removeEventListener(MicrophoneEvents.DataAvailable, onData);
-      clearTimeout(captionTimeout.current);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [connectionState]);
@@ -116,11 +101,12 @@ const App: () => JSX.Element = () => {
       <div className="flex h-full antialiased">
         <div className="flex flex-row h-full w-full overflow-x-hidden">
           <div className="flex flex-col flex-auto h-full">
-            {/* height 100% minus 8rem */}
             <div className="relative w-full h-full">
               {microphone && <Visualizer microphone={microphone} />}
-              <div className="absolute bottom-[8rem]  inset-x-0 max-w-4xl mx-auto text-center">
-                {caption && <span className="bg-black/70 p-8">{caption}</span>}
+              <div className="absolute inset-0 max-w-4xl mx-auto overflow-y-auto p-4">
+                {transcriptions.map((text, index) => (
+                  <p key={index} className="bg-black/70 p-2 mb-2 text-white">{text}</p>
+                ))}
               </div>
             </div>
           </div>
