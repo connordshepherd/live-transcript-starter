93e7e656658aa12375285d61445a54d7e31a2573
Connor Shepherd <connor.d.shepherd@gmail.com>
2024-12-11 20:30:52 -0800

    using form state

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 
+import { useFormState } from 'react-dom'; // Change this line
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { useActionState, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { toast } from 'sonner';
 
 import { AuthForm } from '@/components/auth-form';
@@ -16,7 +17,7 @@ export default function Page() {
   const [email, setEmail] = useState('');
   const [isSuccessful, setIsSuccessful] = useState(false);
 
-  const [state, formAction] = useActionState<RegisterActionState, FormData>(
+  const [state, formAction] = useFormState<RegisterActionState, FormData>(
     register,
     {
       status: 'idle',
